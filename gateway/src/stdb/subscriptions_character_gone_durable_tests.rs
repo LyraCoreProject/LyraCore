02aa40572086aa8b9560104b62d4accb3ffe4e4f
T3 Code <t3code@openai.com>
2026-09-10 00:44:40 +0000

    test(gateway): use declared party destination

--- gateway/src/stdb/subscriptions_character_gone_durable_tests.rs
+++ gateway/src/stdb/subscriptions_character_gone_durable_tests.rs
@@ -282,10 +282,10 @@ fn another_gateway_waits_for_the_transfer_then_cleans_the_deleted_character() {
         standalone.server(),
         standalone.shard_name(),
         "debug_bot_transfer",
-        &["1", "36", "1", "-11208", "1672", "24", "0", "party-test"],
+        &["1", "36", "0", "-11208", "1672", "24", "0", "party-test"],
     );
     standalone.assert_output_success(&output, "transfer Character");
-    crate::world::transfer::run_bot_transfer(&world, 1, 36, 1, "party-test").unwrap();
+    crate::world::transfer::run_bot_transfer(&world, 1, 36, 0, "party-test").unwrap();
     assert!(poll_until(POLL_TIMEOUT, || {
         world.character_by_guid(1).unwrap().is_none()
             && instances.character_by_guid(1).unwrap().is_some()
